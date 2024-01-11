57ae671f813bbb026e6ec12895fcec92e01ced3c
Songyang <aileshang0226@163.com>
2024-01-11 13:49:29 +0800

    init 基本功能完成

---
+++ README.md
@@ -0,0 +1,69 @@
+SSH 自动登录脚本
+---
+
+### 1. 如何使用当前脚本
+
+1. 修改 config.ini ，追加服务器列表
+	- config.init -> # 格式：hostname,user,ip,port,pwd
+2. 赋予脚本可执行权限 `sudo chmod u+x go_ssh.ex pssh.sh`
+3. 使用
+	- 方式一：`./pssh.sh`
+	- 方式二：将 pssh 加入当前用户全局使用
+
+		```bash
+		➜  ~ echo "alias pssh=\"$PWD/pssh.sh\"" >> ~/.zshrc
+		➜  ~ source ~/.zshrc
+		➜  ~ goto
+		```
+
+### 2. 常见问题
+
+#### 2.1. 提示没有 expect 或 spawn 命令
+
+当前脚本主要基于 expect 使用 spawn 实现。
+
+Expect 是一个用来处理交互的工具，通常用于需要手动输入数据的场景，可在脚本中使用 Expect 来实现自动化。
+
+首先使用以下命令检查 expect 是否已安装：
+
+```
+➜  whereis expect
+/usr/bin/expect
+```
+
+如果没有安装，请按照以下步骤：
+
+1. 安装 expect 的依赖 tcl
+
+	```
+	➜  wget https://sourceforge.net/projects/tcl/files/Tcl/8.4.19/tcl8.4.19-src.tar.gz
+	➜  tar zxvf tcl8.4.19-src.tar.gz
+	➜  cd tcl8.4.19/unix && ./configure
+	➜  make
+	➜  make install
+	```
+2. 安装 expect
+
+	```
+	➜  wget http://sourceforge.net/projects/expect/files/Expect/5.45/expect5.45.tar.gz
+	➜  tar zxvf expect5.45.tar.gz
+	➜  cd expect5.45
+	➜  ./configure --with-tcl=/usr/local/lib --with-tclinclude=../tcl8.4.19/generic
+	➜  make
+	➜  make install
+	➜  ln -s /usr/local/bin/expect /usr/bin/expect
+	```
+
+#### 2.2. 特殊字符转义
+
+如果密码中有特殊字符，需要做转义处理，否则使用 expect 的 send 语法是无法发送成功的，具体需要转义的字符如下：
+
+```
+\ ===> \\\
+} ===> \}
+[ ===> \[
+$ ===> \\\$
+` ===> \`
+" ===> \\\"
+~ ===> \\~
+```
---
+++ pssh.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+
+# 服务器列表文件
+BASE_PATH=$(
+    cd "$(dirname "$0")"
+    pwd
+)
+
+echo "$BASE_PATH"
+FILE_SERVER_LIST=$BASE_PATH"/config.ini"
+echo "$FILE_SERVER_LIST"
+
+# 暂存服务器列表，用于登录操作
+CONFIG_ARR=()
+
+# 记录默认分隔符，用于修改后的还原
+IFS_OLD=$IFS
+
+# Set the terminal encoding to UTF-8
+export LC_ALL=C.UTF-8
+
+# 计算字符串的字节长度
+function get_byte_length() {
+    echo -n "$1" | LC_ALL=C.UTF-8 wc -c
+}
+
+# 输出table的row
+function print_table_row() {
+    id="$1"
+    host="$2"
+    port="$3"
+    username="$4"
+    title="$5"
+
+    printf "| %-10s | %-17s | %-10s | %-20s | %-35s \n" "$id" "$host" "$port" "$username" "$title"
+    printf "+------------+-------------------+------------+----------------------+-----------------------------------------------|\n"
+}
+
+# 初始化
+function initialize() {
+    # 检查配置文件
+    if [ ! -f $FILE_SERVER_LIST ]; then
+        echo "Config file not found."
+        exit 1
+    fi
+
+    # 读取配置文件，显示待操作服务器列表
+    clear
+
+    local serverNum=1 # 服务器列表索引
+    local config=()
+
+    print_table_header
+
+    while read line || [ -n "$line" ]; do
+        if [[ ${line} != \#* && "$line" != "" ]]; then
+            IFS=, # 定义读取配置文件时的分隔符
+            config=($line)
+            CONFIG_ARR[$serverNum]=$line
+
+            # 输出一行服务器信息
+            print_table_row "$serverNum" "${config[2]}" "${config[3]}"  "${config[1]}" "${config[0]}"
+
+            # 累加服务器索引，直到配置文件读取完毕
+            serverNum=$(($serverNum + 1))
+        fi
+    done <$FILE_SERVER_LIST
+
+    IFS=$IFS_OLD # 还原分隔符
+
+    echo -en "请输入\033[32m序号\033[0m选择要登录的服务器: "
+    handleChoice
+}
+
+# 过滤掉注释后的配置
+is_comment() {
+    [[ $1 =~ ^[[:space:]]*# ]]
+}
+
+# 输出列表的header
+print_table_header() {
+    printf "+------------+-------------------+------------+----------------------+-----------------------------------------------+\n"
+    printf "| %-10s | %-17s | %-10s | %-20s | %-45s |\n" "ID" "Host" "Port" "Username" "Title"
+    printf "+------------+-------------------+------------+----------------------+-----------------------------------------------|\n"
+}
+
+# 生成最后的长度
+genereatr_header_right() {
+    number="$1"
+
+    # 使用循环生成对应数量的连字符
+    result=""
+    for ((i = 0; i < number; i++)); do
+        result="${result}-"
+    done
+
+    echo $result
+}
+
+# 处理用户输入
+function handleChoice {
+    read -n 2 choice
+    local serverListLength=${#CONFIG_ARR[@]}
+    if [[ "$choice" -lt 1 || "$choice" -gt serverListLength ]]; then
+        echo -en "\n\033[31m无效的序号[ $choice ], 是否重新输入( y 是 | n 否 ):\033[0m"
+        read -n 1 retry
+        if [[ -n "$retry" && "$retry" = "y" ]]; then
+            clear
+            menu
+        else
+            echo ""
+            exit 1
+        fi
+    else
+        sshLogin $choice
+    fi
+}
+
+# 执行 ssh 登录
+function sshLogin {
+
+    IFS=, # 定义读取分隔符
+    local config=(${CONFIG_ARR[$1]})
+
+    # 默认用户 root
+    local user=${config[1]}
+    if [[ $user == "" ]]; then
+        user="root"
+    fi
+
+    # 默认端口号 22
+    local port=${config[3]}
+    if [[ $port == "" ]]; then
+        port="22"
+    fi
+
+    # 开始登录
+    echo -e "\n\n\033[32m==>\033[0m 正在登录【\033[32m${config[0]}\033[0m】，请稍等...\n"
+    sleep 1
+    $(which expect) $BASE_PATH/go_ssh.ex ${config[0]} ${config[2]} $port $user ${config[4]}
+    echo -e "\n\033[32m==>\033[0m 您已退出【\033[32m${config[0]}\033[0m】\n"
+}
+
+# 执行初始化
+initialize
